78dddf0c0153d1b505513d3ff624cfd5eee093d0
Leonardo Nunes <leonardonmattoslopes@gmail.com>
2023-07-29 23:12:10 -0300

    Testes iniciais com movimentos possiveis das pecas

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -2,7 +2,7 @@
 
 namespace tabuleiro
 {
-    internal class Peca
+    abstract class Peca
     {
         public Posicao posicao {  get; set; }
         public Cor cor { get; protected set; }
@@ -27,5 +27,10 @@ namespace tabuleiro
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
+      
+        
     }
 }
